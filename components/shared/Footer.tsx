38caa07b35efd311325bf138b51597ccab48ae45
MOHAMED <mohamed1239455@gmail.com>
2024-03-16 20:13:48 +0200

    copy right

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Footer() {
+  const date = new Date().getFullYear();
   return (
     <footer className=" border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 text-center sm:flex-row">
@@ -13,7 +14,7 @@ export default function Footer() {
             height={38}
           />
         </Link>
-        <p>2023 Evently. All Rights reserved @Mohamed_alfadel</p>
+        <p>{date} Evently. All Rights reserved @Mohamed_alfadel</p>
       </div>
     </footer>
   );
